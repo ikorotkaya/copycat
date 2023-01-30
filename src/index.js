477278032561ff91de87f6331829e82a587c464a
ikorotkaya <irinkakorotkaya@gmail.com>
2023-01-30 19:09:26 +0100

    Delete unused images

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CopyCat from './CopyCat'
 
-const images = {
-  copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
-  quietcat: 'https://content.codecademy.com/courses/React/react_photo_quietcat.png'
-};
-
-
 class CopyCatContainer extends React.Component {
     constructor(props) {
     super(props);
